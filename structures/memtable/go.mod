e34ec32bc893332a737ff38e44e9f96e5a793ff9
Olivera2708 <olivera.radovanovic2002@gmail.com>
2023-01-15 22:16:55 +0100

    added memtable

--- structures/memtable/go.mod
+++
@@ -1,5 +0,0 @@
-module example/vezbe9
-
-go 1.17
-
-require gopkg.in/yaml.v2 v2.4.0 // indirect
---
+++ structures/memtable/memtable.go
@@ -0,0 +1,47 @@
+package main
+
+type Memtable struct {
+	data         SkipList
+	capacity     uint
+	max_capacity uint
+}
+
+func CreateMemtable(height int, max_cap uint, stat uint) *Memtable {
+	skip_list := CreateSkipList(height)
+	memtable := Memtable{*skip_list, 0, stat}
+	return &memtable
+}
+
+func (memtable *Memtable) Add(key string, value []byte, stat uint) {
+	memtable.data.Add(key, value, stat)
+	memtable.capacity++
+}
+
+func (memtable *Memtable) Update(key string, value []byte, stat uint) bool {
+	element := memtable.data.Update(key, value, stat)
+	return element
+}
+
+func (memtable *Memtable) Remove(key string) bool {
+	element := memtable.data.Delete(key)
+	return element
+}
+
+func (Memtable *Memtable) Find(key string) (found bool, value []byte) {
+	found, element := Memtable.data.Find(key)
+	if found {
+		if element.status == 0 {
+			return false, nil
+		} else {
+			return true, element
+		}
+	}
+	return false, nil
+}
+
+func (memtable *Memtable) Flush() {
+	//kada popunjenost bude veca od 80% onda treba da se Flush
+	if float64(memtable.capacity)/float64(memtable.max_capacity)*100 >= 80 {
+		//kod da se prepise u sstable
+	}
+}
