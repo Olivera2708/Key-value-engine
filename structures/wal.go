93d34fe7a8340098130365dedbef89b49b564a21
Sonja-Parabucki <105170152+Sonja-Parabucki@users.noreply.github.com>
2023-01-28 16:08:21 +0100

    Add files via upload

--- structures/wal.go
+++ structures/wal.go
@@ -6,7 +6,6 @@ import (
 	"hash/crc32"
 	"io"
 	"io/fs"
-	"io/ioutil"
 	"log"
 	"os"
 	"strconv"
@@ -15,15 +14,8 @@ import (
 
 /*
    +---------------+-----------------+---------------+---------------+-----------------+-...-+--...--+
-   |    CRC (4B)   | Timestamp (8B) | Tombstone(1B) | Key Size (8B) | Value Size (8B) | Key | Value |
+   |    CRC (4B)   | Timestamp (16B) | Tombstone(1B) | Key Size (8B) | Value Size (8B) | Key | Value |
    +---------------+-----------------+---------------+---------------+-----------------+-...-+--...--+
-   CRC = 32bit hash computed over the payload using CRC
-   Key Size = Length of the Key data
-   Tombstone = If this record was deleted and has a value
-   Value Size = Length of the Value data
-   Key = Key data
-   Value = Value data
-   Timestamp = Timestamp of the operation in seconds
 */
 
 const (
@@ -31,7 +23,7 @@ const (
 	CIFARA_U_NAZIVU = 5 //kod imenovanja segmenata
 
 	CRC_SIZE        = 4
-	TIMESTAMP_SIZE  = 8
+	TIMESTAMP_SIZE  = 16
 	TOMBSTONE_SIZE  = 1
 	KEY_SIZE_SIZE   = 8
 	VALUE_SIZE_SIZE = 8
@@ -50,22 +42,30 @@ func CRC32(data []byte) uint32 {
 
 type WAL struct {
 	segment_size   uint          //maksimalna velicina jednog segmenta
-	segments       []fs.FileInfo //lista za fajlove
+	segments       []fs.DirEntry //lista za fajlove
 	low_water_mark int           //koliko segmenata se ne brise
 }
 
-// mozda provera da li je direktorijum prazan, da onda napravi prvi segmnet koji ce biti prazan fajl?
 func CreateWAL(ss uint, lwm int) *WAL {
-	//iscita segmente=fajlove i da ubaci pokazivace u segments
-	segments, err := ioutil.ReadDir(DIRNAME)
-	if err != nil {
+
+	segments, err := os.ReadDir(DIRNAME)
+	if len(segments) == 0 {
+
+		//OVO NE RADI ZA PRAZAN DIR <<<<
+
+		file, err := os.OpenFile(DIRNAME+"/wal_00001.log", os.O_CREATE, 0666)
+		if err != nil {
+			log.Fatal(err)
+		}
+		file.Close()
+	} else if err != nil {
 		log.Fatal(err)
 	}
 
 	return &WAL{ss, segments, lwm}
 }
 
-func IsFull(filename string) uint {
+func NumberOfRecords(filename string) uint {
 	file, err := os.OpenFile(filename, os.O_RDONLY, 0666)
 	if err != nil {
 		log.Fatal(err)
@@ -101,8 +101,7 @@ func IsFull(filename string) uint {
 	return k
 }
 
-// drugi tipovi parametara?
-func (wal *WAL) Add(key string, val string) {
+func (wal *WAL) Add(key string, valb []byte, flag byte) {
 
 	//zapis pravis po formatu gore
 	keyb := []byte(key)
@@ -110,16 +109,15 @@ func (wal *WAL) Add(key string, val string) {
 	key_size := make([]byte, KEY_SIZE_SIZE)
 	binary.LittleEndian.PutUint64(key_size, key_len)
 
-	valb := []byte(val)
 	val_len := uint64(len(valb))
 	val_size := make([]byte, VALUE_SIZE_SIZE)
 	binary.LittleEndian.PutUint64(val_size, val_len)
 
-	timestamp := []byte(time.Now().String()) // <<<<<------------
-	//TREBA DA BUDE TACNO 8 BAJTOVA
+	timestamp := make([]byte, TIMESTAMP_SIZE)
+	binary.LittleEndian.PutUint16(timestamp, uint16(time.Now().Unix()))
 
 	tombstone := make([]byte, 1)
-	tombstone[0] = byte(1)
+	tombstone[0] = flag
 
 	record := append(timestamp, tombstone...)
 	record = append(record, key_size...)
@@ -137,8 +135,8 @@ func (wal *WAL) Add(key string, val string) {
 	s := wal.segments[n-1]
 
 	//proveri da li ima slobodnog mesta
-	if wal.segment_size == IsFull(DIRNAME+"/"+s.Name()) {
-		rbr, err := strconv.ParseUint(DIRNAME+"/"+s.Name()[4:10], 10, 32) //kako radi sjasing stringa ovde????
+	if wal.segment_size == NumberOfRecords(DIRNAME+"/"+s.Name()) {
+		rbr, err := strconv.ParseUint(s.Name()[4:9], 10, 32)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -149,19 +147,20 @@ func (wal *WAL) Add(key string, val string) {
 		}
 
 		new_file_name := "wal_" + broj + ".log"
-		s, err := os.OpenFile(DIRNAME+"/"+new_file_name, os.O_CREATE, 0666) //dozvole
+		s, err := os.OpenFile(DIRNAME+"/"+new_file_name, os.O_CREATE, 0666)
 		s.Close()
 		if err != nil {
 			log.Fatal(err)
 		}
-		ss := make([]fs.FileInfo, 1)
-		ss[0], _ = os.Stat(DIRNAME + "/" + s.Name())
-		wal.segments = append(wal.segments, ss...)
+		wal.segments, err = os.ReadDir(DIRNAME)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 	n = len(wal.segments)
 	s = wal.segments[n-1]
 
-	file, err := os.OpenFile(DIRNAME+"/"+s.Name(), os.O_RDWR, 0666) //dozvole?
+	file, err := os.OpenFile(DIRNAME+"/"+s.Name(), os.O_RDWR, 0666)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -183,12 +182,12 @@ func (wal *WAL) ReadAll() {
 	vsb := make([]byte, VALUE_SIZE_SIZE)
 
 	for _, seg := range wal.segments {
-		file, err := os.OpenFile(seg.Name(), os.O_RDONLY, 0666)
-		defer file.Close()
+		file, err := os.OpenFile(DIRNAME+"/"+seg.Name(), os.O_RDONLY, 0666)
 
 		if err != nil {
 			log.Fatal(err)
 		}
+		defer file.Close()
 
 		jos := true
 
@@ -214,6 +213,8 @@ func (wal *WAL) ReadAll() {
 			file.Read(key)
 			val := make([]byte, val_size)
 			file.Read(val)
+
+			//
 			fmt.Println(key)
 			fmt.Println(val)
 			fmt.Println()
@@ -223,6 +224,13 @@ func (wal *WAL) ReadAll() {
 			//provera crc?
 			data := make([]byte, TIMESTAMP_SIZE+TOMBSTONE_SIZE+
 				KEY_SIZE_SIZE+VALUE_SIZE_SIZE+key_size+val_size)
+
+			data = append(data, time...)
+			data = append(data, tomb...)
+			data = append(data, ksb...)
+			data = append(data, vsb...)
+			data = append(data, key...)
+			data = append(data, val...)
 
 			if crc != CRC32(data) {
 				fmt.Println("Korumpirani podaci")
@@ -234,7 +242,7 @@ func (wal *WAL) ReadAll() {
 func (wal *WAL) Flush() {
 	n := len(wal.segments)
 	if n > wal.low_water_mark {
-		new := make([]fs.FileInfo, wal.low_water_mark)
+		new := make([]fs.DirEntry, wal.low_water_mark)
 
 		j := 0
 		for i := wal.low_water_mark - 1; i < n; i++ {
@@ -244,7 +252,7 @@ func (wal *WAL) Flush() {
 
 		//obrise suvisne
 		for i := 0; i < wal.low_water_mark-1; i++ {
-			err := os.Remove(wal.segments[i].Name())
+			err := os.Remove(DIRNAME + "/" + wal.segments[i].Name())
 			if err != nil {
 				log.Fatal(err)
 			}
@@ -258,7 +266,7 @@ func (wal *WAL) Flush() {
 			}
 
 			new_file_name := "wal_" + broj + ".log"
-			err := os.Rename(new[i].Name(), new_file_name)
+			err := os.Rename(DIRNAME+"/"+new[i].Name(), DIRNAME+"/"+new_file_name)
 			if err != nil {
 				log.Fatal(err)
 			}
